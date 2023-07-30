da6bdb297384a5ccaa3d506a4c4b13b2573acfda
juliocribeiro <juliocribeiro00@gmail.com>
2023-07-30 16:58:36 -0300

    segundo codigo

---
+++ codigo 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+void exibirNumerosPares ();
+
+int i = 0;
+
+for (int i = 0; i <= 10; ++ i)
+
+if (i % 2 == 0)
+
+cout << "numero par:" << i << endl;
+
+}
---
+++ codigo 3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+int imprimirNumeros;
+
+int i= 1;
+
+for (int i = 1; i <= 10; i++)
+
+cout << "Numero: " << i << endl;
+
+}
